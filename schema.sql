f0e1a14217c513817535b2a007c4285d2933a294
Alisa <16126180@bjtu.edu.cn>
2016-10-31 12:45:41 +0000

    更新 'schema.sql'

--- schema.sql
+++ schema.sql
@@ -2,5 +2,6 @@ drop table if exists entries;
 create table entries (
   id integer primary key autoincrement,
   name text not null,
-  password text not null
+  password text not null,
+  salt text not null
 );
